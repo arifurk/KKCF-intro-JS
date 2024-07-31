4244cb084f032289dae8705c2aa2b06bb7c40247
arifurk <arifurkhan.rk7@gmail.com>
2024-07-31 18:02:23 +0000

    madlibs

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} // taken from stack overflow at https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 
-//console log text, expression 
-let name = "arifur"
-console.log(name)
+let noun = prompt('Enter a name')
+let number = getRandomInt(1, 5)
+let number2 = number + 2
 
-// Create a variable to hold your name as a string
+let boolean = true
+if (getRandomInt(1,2) == 1) {
+    boolean = false
+} 
 
-//Concatenate your name variable into a message and log it to the console.
-//Example: “Hello, Zach!” 
+let sen1 = 'There was a person named ' + noun + '. '
+let sen2 = 'They had ' + number + ' shovels. '
+let sen3 = 'For some reason, they wanted to dig up ' + number2 + ' holes in their yard. '
 
-//console log your name variable
+let sen4 = "And so they did, because who would've stopped them. "
+if (boolean) {
+    sen4 = 'But all their shovels broke while trying to dig those holes. '
+}
+let sen5 = 'Is there something to take away from this? Probably not. '
 
-//Re-assign the value of your name variable below its declaration and console log it again
+let final = sen1 + sen2 + sen3 + sen4 + sen5
+console.log(final)
 
-//practice math operators
-
-// boolean - true or false
-
-//create null and undefined
-
-//mix data types
-
-//string methods
-
-//link up html file and create prompt and save value
-let username = prompt('whats your name')
-console.log('hello ' + username)
-
-//access the DOM
+document.getElementById('story').innerHTML = "<h3>"+final+"</h3>" // taken from https://codegyan.in/articles/how-to-convert-a-string-to-html-using-javascript.htm
